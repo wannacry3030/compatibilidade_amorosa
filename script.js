1651a671247d2f977f02f51a092d5f6f75417d6d
wannacry3030 <wannacry.bat@proton.me>
2023-03-16 15:20:55 -0300

    att

--- script.js
+++ script.js
@@ -11,7 +11,11 @@ form.addEventListener("submit", function (event) {
   const compatibilidade = calcularCompatibilidade(seuNome, nomeOutraPessoa);
 
   // Exibição do resultado
-  resultado.innerHTML = `A compatibilidade entre ${seuNome} e ${nomeOutraPessoa} é de ${compatibilidade}%`;
+  const resultadoContainer = document.querySelector("#resultado-container");
+  resultadoContainer.innerHTML = `
+    <h2>Compatibilidade Amorosa</h2>
+    <p>A compatibilidade entre ${seuNome} e ${nomeOutraPessoa} é de ${compatibilidade}%</p>
+  `;
 });
 
 // Função para calcular a compatibilidade
