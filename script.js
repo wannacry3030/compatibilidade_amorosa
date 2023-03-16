550ac0a8ef95420a68e5431de75952767cc00fb3
wannacry3030 <wannacry.bat@proton.me>
2023-03-16 14:46:30 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const form = document.querySelector("form");
+const resultado = document.querySelector("#resultado");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const seuNome = document.querySelector("#seu-nome").value;
+  const nomeOutraPessoa = document.querySelector("#nome-outra-pessoa").value;
+
+  // Cálculo da compatibilidade
+  const compatibilidade = calcularCompatibilidade(seuNome, nomeOutraPessoa);
+
+  // Exibição do resultado
+  resultado.innerHTML = `A compatibilidade entre ${seuNome} e ${nomeOutraPessoa} é de ${compatibilidade}%`;
+});
+
+// Função para calcular a compatibilidade
+function calcularCompatibilidade(nome1, nome2) {
+  const soma = somaDosValoresDosNomes(nome1) + somaDosValoresDosNomes(nome2);
+  const compatibilidade = soma % 101;
+  return compatibilidade;
+}
+
+// Função para somar os valores de cada letra de um nome
+function somaDosValoresDosNomes(nome) {
+  let soma = 0;
+  for (let i = 0; i < nome.length; i++) {
+    soma += nome.charCodeAt(i);
+  }
+  return soma;
+}
