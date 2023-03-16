cc832afbf6facad6d71a37a9f7c8454a2555aefb
wannacry3030 <wannacry.bat@proton.me>
2023-03-16 17:26:49 -0300

    att

--- script.js
+++ script.js
@@ -7,5 +7,6 @@ form.addEventListener("submit", function (e) {
   const nomeOutraPessoa = document.querySelector("#nome-outra-pessoa").value;
   const porcentagem = Math.floor(Math.random() * 101);
 
-  resultado.innerHTML = `<p>A chance de ${seuNome} e ${nomeOutraPessoa} darem certo é de ${porcentagem}%</p>`;
+  resultado.innerHTML = `<p>A chance de ${seuNome} e ${nomeOutraPessoa} darem certo é de ${porcentagem}% 💞</p>`;
+  //
 });
