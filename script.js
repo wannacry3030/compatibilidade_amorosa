002c53ed9582c4c8d90d18beaeec675fc1813511
wannacry3030 <wannacry.bat@proton.me>
2023-03-16 16:33:33 -0300

    att

--- script.js
+++ script.js
@@ -1,31 +1,13 @@
 const form = document.querySelector("form");
-const resultado = document.querySelector("#resultado");
+const seuNomeInput = document.querySelector("#seu-nome");
+const nomeOutraPessoaInput = document.querySelector("#nome-outra-pessoa");
+const resultadoDiv = document.querySelector("#resultado");
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  const seuNome = document.querySelector("#seu-nome").value;
-  const nomeOutraPessoa = document.querySelector("#nome-outra-pessoa").value;
+  const seuNome = seuNomeInput.value;
+  const nomeOutraPessoa = nomeOutraPessoaInput.value;
 
-  // Cálculo da compatibilidade
-  const compatibilidade = calcularCompatibilidade(seuNome, nomeOutraPessoa);
-
-  // Exibição do resultado
-  resultado.innerHTML = `A compatibilidade entre ${seuNome} e ${nomeOutraPessoa} é de ${compatibilidade}%`;
+  resultadoDiv.innerText = `Olá ${seuNome} e ${nomeOutraPessoa}, vocês são compatíveis!`;
 });
-
-// Função para calcular a compatibilidade
-function calcularCompatibilidade(nome1, nome2) {
-  const soma = somaDosValoresDosNomes(nome1) + somaDosValoresDosNomes(nome2);
-  const compatibilidade = soma % 101;
-  return compatibilidade;
-}
-
-// Função para somar os valores de cada letra de um nome
-function somaDosValoresDosNomes(nome) {
-  let soma = 0;
-  for (let i = 0; i < nome.length; i++) {
-    soma += nome.charCodeAt(i);
-  }
-  return soma;
-}
